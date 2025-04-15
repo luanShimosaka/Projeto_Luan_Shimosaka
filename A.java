551459c1dbcd866de0632d335b8d8ee04ff1724a
LuanShimosaka <luan.shimosaka@gmail.com>
2025-04-15 20:37:48 -0300

    Classes A e B

---
+++ A.java
@@ -0,0 +1,33 @@
+public class A {
+    private int A1;
+    private float A2;
+
+    public A(int a1, float a2) {
+        this.A1 = a1;
+        this.A2 = a2;
+    }
+
+    public int getA1() {
+        return A1;
+    }
+
+    public void setA1(int a1) {
+        A1 = a1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float a2) {
+        A2 = a2;
+    }
+
+    public void MA1() {
+        System.out.println("MA1");
+    }
+
+    public void MA2() {
+        System.out.println("MA2");
+    }
+}
