41504198adcc20d448077feb264dadb428a706c6
LuanShimosaka <luan.shimosaka@gmail.com>
2025-04-15 22:29:20 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,17 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    public C(String c1, int c2) {
+        this.C1 = c1;
+        this.C2 = c2;
+    }
+
+    public void MC1() {
+        System.out.println("MC1");
+    }
+
+    public void MC2() {
+        System.out.println("MC2");
+    }
+}
